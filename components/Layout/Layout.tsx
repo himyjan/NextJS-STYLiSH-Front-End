a31dcddf86baf3fdd451326b8c2121a0b4944beb
Luka <srps8912@gmail.com>
2023-02-16 15:36:29 +0800

    Feat: Add footer

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
+import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 
 const Layout = ({ children }: { children: JSX.Element }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main className="min-h-[calc(100vh-308px)]">{children}</main>
+      <Footer />
     </>
   );
 };
