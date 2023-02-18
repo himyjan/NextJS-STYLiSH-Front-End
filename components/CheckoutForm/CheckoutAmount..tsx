53b83e57731e8f4099c4b7369c8d58a90fcafa50
Luka <srps8912@gmail.com>
2023-02-18 23:53:50 +0800

    Style: Add recipient inputs

--- components/CheckoutForm/CheckoutAmount..tsx
+++ components/CheckoutForm/CheckoutAmount..tsx
@@ -1,17 +1,38 @@
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 
-const FREIGHT = 30;
-
-const CheckoutAmount = () => {
+const CheckoutAmount = ({ freight }: { freight: number }) => {
   const cart = useSelector((state: RootState) => state.cart);
 
   return (
-    <div className="flex flex-col">
-      <div className="flex">
-        <div>總金額</div>
-        <div>NT.</div>
-        <div>{cart.amount}</div>
+    <div className="flex flex-col items-end gap-[20px] mb-[36px]">
+      <div className="flex items-center w-[240px]">
+        <div className="text-[16px] leading-[19px] text-light-black flex-1">
+          總金額
+        </div>
+        <div className="text-[16px] leading-[19px] text-light-black">NT.</div>
+        <div className="text-[30px] leading-[36px] text-light-black">
+          {cart.amount}
+        </div>
+      </div>
+      <div className="flex items-center w-[240px]">
+        <div className="text-[16px] leading-[19px] text-light-black flex-1">
+          運費
+        </div>
+        <div className="text-[16px] leading-[19px] text-light-black">NT.</div>
+        <div className="text-[30px] leading-[36px] text-light-black">
+          {freight}
+        </div>
+      </div>
+      <div className="border-b border-light-black w-[240px]" />
+      <div className="flex items-center w-[240px]">
+        <div className="text-[16px] leading-[19px] text-light-black flex-1">
+          運費
+        </div>
+        <div className="text-[16px] leading-[19px] text-light-black">NT.</div>
+        <div className="text-[30px] leading-[36px] text-light-black">
+          {cart.amount + freight}
+        </div>
       </div>
     </div>
   );
--- components/CheckoutForm/CheckoutForm.tsx
+++ components/CheckoutForm/CheckoutForm.tsx
@@ -2,6 +2,7 @@ import tappay from "@/utils/tappay";
 import { useEffect, useRef, useState } from "react";
 import CheckoutAmount from "./CheckoutAmount.";
 
+const FREIGHT = 30;
 
 const CheckoutForm = () => {
   const cardNumberRef = useRef<HTMLDivElement>(null);
@@ -29,37 +30,155 @@ const CheckoutForm = () => {
   return (
     <div className="w-full flex flex-col pb-[35px]">
       <form onSubmit={checkOutHandler}>
-        <div className="w-full text-[16px] leading-[19px] font-bold pb-[10px] mb-[20px] border-b border-light-black">
-          訂購資料
-        </div>
-        <div className="w-full flex flex-col">
-          <div className="cart-form-input-container">
-            <label htmlFor="card-number" className="cart-form-input-label">
+        <div className="w-full flex flex-col gap-[20px] mb-[24px]">
+          <div className="w-full text-[16px] leading-[19px] font-bold pb-[10px] mb-[20px] border-b border-light-black">
+            訂購資料
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label
+              htmlFor="recipient"
+              className="text-[14px] leading-[17px] text-light-black"
+            >
+              收件人姓名
+            </label>
+            <input
+              className="w-full border border-light-grey-4 h-[32px] rounded-[8px] px-[5px] overflow-hidden"
+              id="recipient"
+            />
+            <div className="text-[14px] leading-[17px] text-brown">
+              務必填寫完整收件人姓名，避免包裹無法順利簽收
+            </div>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label
+              htmlFor="mobilePhone"
+              className="text-[14px] leading-[17px] text-light-black"
+            >
+              手機
+            </label>
+            <input
+              className="w-full border border-light-grey-4 h-[32px] rounded-[8px] px-[5px] overflow-hidden"
+              id="mobilePhone"
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label
+              htmlFor="address"
+              className="text-[14px] leading-[17px] text-light-black"
+            >
+              地址
+            </label>
+            <input
+              className="w-full border border-light-grey-4 h-[32px] rounded-[8px] px-[5px] overflow-hidden"
+              id="address"
+            />
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label
+              htmlFor="email"
+              className="text-[14px] leading-[17px] text-light-black"
+            >
+              Email
+            </label>
+            <input
+              className="w-full border border-light-grey-4 h-[32px] rounded-[8px] px-[5px] overflow-hidden"
+              id="email"
+            />
+          </div>
+          <div className="flex flex-wrap gap-[10px]">
+            <label
+              htmlFor="deliverTime"
+              className="text-[14px] leading-[17px] text-light-black w-full"
+            >
+              配送時間
+            </label>
+            <div className="flex items-center">
+              <input
+                name="deliverTime"
+                id="deliverTime-1"
+                value="morning"
+                type="radio"
+              />
+              <label
+                htmlFor="deliverTime"
+                className="text-[14px] leading-[26px] ml-[5px]"
+              >
+                08:00-12:00
+              </label>
+            </div>
+            <div className="flex items-center">
+              <input
+                name="deliverTime"
+                id="deliverTime-2"
+                value="afternoon"
+                type="radio"
+              />
+              <label
+                htmlFor="deliverTime"
+                className="text-[14px] leading-[26px] ml-[5px]"
+              >
+                14:00-18:00
+              </label>
+            </div>
+            <div className="flex items-center">
+              <input
+                name="deliverTime"
+                id="deliverTime-3"
+                value="anytime"
+                type="radio"
+              />
+              <label
+                htmlFor="deliverTime"
+                className="text-[14px] leading-[26px] ml-[5px]"
+              >
+                不指定
+              </label>
+            </div>
+          </div>
+          <div className="w-full text-[16px] leading-[19px] font-bold pb-[10px] mb-[20px] border-b border-light-black">
+            付款資料
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label
+              htmlFor="card-number"
+              className="text-[14px] leading-[17px] text-light-black"
+            >
               信用卡號碼
             </label>
-            <div className="tpfield" id="card-number" ref={cardNumberRef} />
+            <div
+              className="w-full border border-light-grey-4 h-[32px] rounded-[8px] px-[5px] overflow-hidden"
+              id="card-number"
+              ref={cardNumberRef}
+            />
           </div>
-          <div className="cart-form-input-container">
+          <div className="flex flex-col gap-[10px]">
             <label
               htmlFor="card-expiration-date"
-              className="cart-form-input-label"
+              className="text-[14px] leading-[17px] text-light-black"
             >
               有效期限
             </label>
             <div
-              className="tpfield"
+              className="w-full border border-light-grey-4 h-[32px] rounded-[8px] px-[5px] overflow-hidden"
               id="card-expiration-date"
               ref={cardExpirationDateRef}
             />
           </div>
-          <div className="cart-form-input-container">
-            <label htmlFor="card-ccv" className="cart-form-input-label">
+          <div className="flex flex-col gap-[10px]">
+            <label
+              htmlFor="card-ccv"
+              className="text-[14px] leading-[17px] text-light-black"
+            >
               安全碼
             </label>
-            <div className="tpfield" id="card-ccv" ref={cardCCVRef} />
+            <div
+              className="w-full border border-light-grey-4 h-[32px] rounded-[8px] px-[5px] overflow-hidden"
+              id="card-ccv"
+              ref={cardCCVRef}
+            />
           </div>
         </div>
-        <CheckoutAmount />
+        <CheckoutAmount freight={FREIGHT} />
         <button className="bg-black text-white w-full h-[44px] text-center text-[16px]leading-[30px]">
           確認付款
         </button>
