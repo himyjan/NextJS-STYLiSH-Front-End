b7a9b8be97e7ff13e5527a35afc678deeb8ae26c
Luka <srps8912@gmail.com>
2023-02-18 23:53:00 +0800

    Feat: Update tappay

--- components/CheckoutForm/CheckoutForm.tsx
+++ components/CheckoutForm/CheckoutForm.tsx
@@ -1,85 +1,17 @@
 import tappay from "@/utils/tappay";
 import { useEffect, useRef, useState } from "react";
 import CheckoutAmount from "./CheckoutAmount.";
-const APP_ID = process.env.NEXT_PUBLIC_TAPPAY_ID;
-const APP_KEY = process.env.NEXT_PUBLIC_TAPPAY_KEY;
 
-let isInitial = true;
 
 const CheckoutForm = () => {
-  const [ready, setReady] = useState(false);
   const cardNumberRef = useRef<HTMLDivElement>(null);
   const cardExpirationDateRef = useRef<HTMLDivElement>(null);
   const cardCCVRef = useRef<HTMLDivElement>(null);
-
-  // const TPDSetupHandler = () => [
-  //   window.TPDirect.card.setup({
-  //     // Display ccv field
-  //     fields: {
-  //       number: {
-  //         // css selector
-  //         element: "#card-number",
-  //         placeholder: "**** **** **** ****",
-  //       },
-  //       expirationDate: {
-  //         // DOM object
-  //         element: document.getElementById("card-expiration-date"),
-  //         placeholder: "MM / YY",
-  //       },
-  //       ccv: {
-  //         element: "#card-ccv",
-  //         placeholder: "ccv",
-  //       },
-  //     },
-
-  //     styles: {
-  //       // Style all elements
-  //       input: {
-  //         color: "gray",
-  //       },
-  //       // style valid state
-  //       ".valid": {
-  //         color: "green",
-  //       },
-  //       // style invalid state
-  //       ".invalid": {
-  //         color: "red",
-  //       },
-  //       // Media queries
-  //       // Note that these apply to the iframe, not the root window.
-  //       "@media screen and (max-width: 400px)": {
-  //         input: {
-  //           color: "orange",
-  //         },
-  //       },
-  //     },
-  //     // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
-  //     isMaskCreditCardNumber: true,
-  //     maskCreditCardNumberRange: {
-  //       beginIndex: 6,
-  //       endIndex: 11,
-  //     },
-  //   }),
-  // ];
-
-  // const tapPayHandler = () => {
-  //   const tapPayState = window.TPDirect.card.getTappayFieldsStatus();
-  //   return tapPayState.canGetPrime;
-  // };
 
   const checkOutHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("checkout");
   };
-
-  // useEffect(() => {
-  //   if (isInitial) {
-  //     window.TPDirect.setupSDK(APP_ID, APP_KEY, "sandbox");
-  //     TPDSetupHandler();
-  //     isInitial = false;
-  //     console.log("setup");
-  //   }
-  // }, []);
 
   useEffect(() => {
     const setupTappay = async () => {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* <script
-          src="https://js.tappaysdk.com/sdk/tpdirect/v5.14.0"
-          async
-        ></script> */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
