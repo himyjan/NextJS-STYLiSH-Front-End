3672b8418ddb1a2c61c4b63e7638f33c3dd01391
Luka <srps8912@gmail.com>
2023-02-18 21:35:58 +0800

    Fix: Filename Errpr

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "@/context/authContext";
+import { useAuth } from "@/context/AuthContext";
 
 const Member = () => {
   const { user, isLogin, login, logout, loading } = useAuth();
