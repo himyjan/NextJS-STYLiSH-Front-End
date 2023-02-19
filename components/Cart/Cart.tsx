1433c55e7d6795512fa3dc2eaf68d2c84010f607
Luka <srps8912@gmail.com>
2023-02-19 22:59:43 +0800

    Style:RWD layout of checkout page (bigger than 1280px)

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -21,15 +21,22 @@ const Cart = () => {
   }, [cart]);
 
   return (
-    <div className="pt-[20px]">
+    <div className="pt-[20px] xl:w-[1160px] xl:pt-[51px] xl:mb-[50px]">
       <div className="flex flex-col">
         <div className="flex mb-[10px]">
-          <div className="text-1 leading-[19px] font-bold">{`購物車(${cart.totalQuantity})`}</div>
-          <div className="hidden">數量</div>
-          <div className="hidden">單價</div>
-          <div className="hidden">小計</div>
+          <div className="text-1 leading-[19px] font-bold xl:flex-1">{`購物車(${cart.totalQuantity})`}</div>
+          <div className="hidden text-[16px] leading-[19px] w-[192px] xl:block">
+            數量
+          </div>
+          <div className="hidden text-[16px] leading-[19px] w-[192px] xl:block">
+            單價
+          </div>
+          <div className="hidden text-[16px] leading-[19px] w-[192px] xl:block">
+            小計
+          </div>
+          <div className="hidden xl:block xl:ml-[52px]" />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col xl:border xl:border-light-grey-4 xl:px-[30px] xl:py-[40px]">
           {cart.items.map((item) => (
             <CartItem
               key={`cart-item-${item.id}-${item.colorCode}-${item.size}`}
--- components/Cart/CartItem.tsx
+++ components/Cart/CartItem.tsx
@@ -19,8 +19,8 @@ const CartItem = ({ data }: { data: Cart }) => {
   };
 
   return (
-    <div className="pt-[10px] pb-[20px] border-t border-light-black relative">
-      <div className="flex gap-x-[10px] mb-[20px]">
+    <div className="pt-[10px] pb-[20px] border-t border-light-black relative xl:flex xl:items-center xl:p-[0px] xl:border-none">
+      <div className="flex gap-x-[10px] mb-[20px] xl:flex-1">
         <div className="w-[114px] h-[152px] relative flex-shrink-0">
           <Image
             width="0"
@@ -33,32 +33,32 @@ const CartItem = ({ data }: { data: Cart }) => {
           />
         </div>
         <div>
-          <div className="text-[14px] leading-[17px] text-black mb-[20px]">
+          <div className="text-[14px] leading-[17px] text-black mb-[20px] xl:text-[16px] xl:leading-[19px] xl:mb-[18px]">
             {data.title}
           </div>
-          <div className="text-[14px] leading-[17px] text-light-black mb-[24px]">
+          <div className="text-[14px] leading-[17px] text-light-black mb-[24px] xl:text-[16px] xl:leading-[19px] xl:mb-[22px]">
             {data.id}
           </div>
-          <div className="flex h-[17px] items-center mb-[12px]">
-            <div className="text-[14px] leading-[17px] text-light-black pr-[8px] mr-[8px] border-r border-light-black">
+          <div className="flex h-[17px] items-center mb-[12px] xl:mb-[10px]">
+            <div className="text-[14px] leading-[17px] text-light-black pr-[8px] mr-[8px] border-r border-light-black xl:text-[16px] xl:leading-[19px]">
               顏色
             </div>
-            <div className="text-[14px] leading-[17px] text-light-black">
+            <div className="text-[14px] leading-[17px] text-light-black xl:text-[16px] xl:leading-[19px]">
               {data.colorName}
             </div>
           </div>
           <div className="flex h-[17px] items-center">
-            <div className="text-[14px] leading-[17px] text-light-black pr-[8px] mr-[8px] border-r border-light-black">
+            <div className="text-[14px] leading-[17px] text-light-black pr-[8px] mr-[8px] border-r border-light-black xl:text-[16px] xl:leading-[19px]">
               尺寸
             </div>
-            <div className="text-[14px] leading-[17px] text-light-black">
+            <div className="text-[14px] leading-[17px] text-light-black xl:text-[16px] xl:leading-[19px]">
               {data.size}
             </div>
           </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center gap-y-[12px]">
-        <div className="flex items-center justify-center w-full">
+        <div className="flex items-center justify-center w-full xl:hidden">
           <div className="text-[14px] leading-[17px] text-center flex-1">
             數量
           </div>
@@ -70,7 +70,7 @@ const CartItem = ({ data }: { data: Cart }) => {
           </div>
         </div>
         <div className="flex items-center justify-center w-full">
-          <div className="text-[14px] leading-[17px] text-center flex-1">
+          <div className="text-[14px] leading-[17px] text-center flex-1  xl:w-[192px] xl:text-[16px] xl:leading-[19px]">
             <select
               className="text-light-black bg-light-grey-5 w-[80px] h-[30px] px-[10px] border border-light-grey-3 rounded"
               value={data.quantity}
@@ -92,13 +92,13 @@ const CartItem = ({ data }: { data: Cart }) => {
               )}
             </select>
           </div>
-          <div className="text-[14px] leading-[17px] text-center flex-1">{`TWD.${data.price}`}</div>
-          <div className="text-[14px] leading-[17px] text-center flex-1">{`TWD.${
+          <div className="text-[14px] leading-[17px] text-center flex-1 xl:w-[192px] xl:text-[16px] xl:leading-[19px]">{`TWD.${data.price}`}</div>
+          <div className="text-[14px] leading-[17px] text-center flex-1 xl:w-[192px] xl:text-[16px] xl:leading-[19px]">{`TWD.${
             data.price * data.quantity
           }`}</div>
         </div>
       </div>
-      <div className="w-[44px] h-[44px] absolute top-0 right-0">
+      <div className="w-[44px] h-[44px] absolute top-0 right-0 xl:relative xl:ml-[52px]">
         <button
           className="bg-cart-remove w-[44px] h-[44px] bg-cover bg-center hover:bg-cart-remove-hover"
           onClick={removeItemHandler}
