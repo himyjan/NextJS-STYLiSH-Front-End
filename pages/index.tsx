0e3a182ad43e5810bb9e477198100d92206760f5
Luka <srps8912@gmail.com>
2023-02-20 10:16:02 +0800

    Feat: Fetch first page of products data by GetServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,16 @@ import Products from "@/components/Products/Products";
 import Head from "next/head";
 import api from "@/utils/api";
 import { GetServerSideProps } from "next";
+import { ProductData } from "@/types/types";
+import { ParsedUrlQuery } from "querystring";
 
-export default function Home({ carouselData }: { carouselData: Carousel[] }) {
+export default function Home({
+  carouselData,
+  productsData,
+}: {
+  carouselData: Carousel[];
+  productsData: { data: ProductData[]; nextPaging: number | null };
+}) {
   return (
     <>
       <Head>
@@ -14,20 +22,35 @@ export default function Home({ carouselData }: { carouselData: Carousel[] }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Carousel carouselData={carouselData} />
-      <Products />
+      <Products firstPageData={productsData} />
     </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const fetchDataHandler = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const url = query as ParsedUrlQuery;
+
+  const fetchCarouselDataHandler = async () => {
     const response = await api.getCampaigns();
     const data = response.data as Carousel[];
     return data;
   };
-  const carouselData = await fetchDataHandler();
+
+  const fetchFirstPageDataHandler = async () => {
+    const category = url.category || "all";
+    const response = await api.getProducts(category as string, 0);
+    const data = response.data as ProductData[];
+    const nextPaging = response.next_paging as number | undefined;
+    if (nextPaging === undefined) {
+      return { data, nextPaging: null };
+    }
+    return { data, nextPaging };
+  };
+
+  const carouselData = await fetchCarouselDataHandler();
+  const productsData = await fetchFirstPageDataHandler();
 
   return {
-    props: { carouselData },
+    props: { carouselData, productsData },
   };
 };
